d66f1897b48c0f53868188e7bd2826e437bb2383
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-16 01:11:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(collection)
+  new_collection = []
+  i = 0
+while i < collection.length
+new_collection <<
+yield(collection[i])
+i+=1
+end
+new_collection
+end
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]){|item|item}
 
