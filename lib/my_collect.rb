7e452f44776d54c5324a7f8f762fec7016c03002
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-16 00:54:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(collection)
+  new_collection = []
+  i = 0
+while i < collection.length
+yield new_collection << collection[|name|] 
+i+=1
+end
+new_collection
+end
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name| name
+end
 
 
